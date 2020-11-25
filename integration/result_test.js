abe5a2ba5e8c3012cd972099a5a4909181e6bbfa
lmurature <lucio.murature@mercadolibre.com>
2020-11-25 14:03:32 -0300

    changed option

--- integration/result_test.js
+++ integration/result_test.js
@@ -2,6 +2,6 @@ Feature('result');
 
 Scenario('test voting app result view', ({ I }) => {
     I.amOnPage('http://159.203.190.97:5001/');
-    I.see('CATS');
+    I.see('BEARS');
     I.see('DOGS');
 });
--- integration/vote_test.js
+++ integration/vote_test.js
@@ -2,8 +2,8 @@ Feature('vote');
 
 Scenario('test voting app vote view', ({ I }) => {
     I.amOnPage('http://159.203.190.97:5000/');
-    I.see('CATS');
+    I.see('BEARS');
     I.see('DOGS');
-    I.see('Cats vs Dogs!')
+    I.see('Bears vs Dogs!')
     I.see('(Tip: you can change your vote)');
 });
